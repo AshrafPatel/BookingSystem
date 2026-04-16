f1bc93a75048d782aecbf79635cbf60e7722bf4f
AshrafPatel <4shr4f@live.co.uk>
2026-04-16 19:35:06 +0100

    Reupdated my models and changed name to be more consistent

--- BookingSystem.Domain/Entities/CustomerProfile.cs
+++ BookingSystem.Domain/Entities/CustomerProfile.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BookingSystem.Domain.Entities
 {
-    public class StaffProfile
+    public class CustomerProfile
     {
+        [Key]
         public Guid Id { get; set; }
-        public Guid UserId { get; set; }
-        public User? User { get; set; }
-        public string? DisplayName { get; set; } //What sort of Doctor, Therapist, etc
         public ICollection<TimeSlot>? TimeSlots { get; set; }
-
     }
 }
--- BookingSystem.Domain/Entities/StaffProfile.cs
+++ BookingSystem.Domain/Entities/StaffProfile.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BookingSystem.Domain.Entities
 {
-    public class CustomerProfile
+    public class StaffProfile
     {
+        [Key]
         public Guid Id { get; set; }
-        public Guid UserId { get; set; }
-        public User? User { get; set; }
+        public string? DisplayName { get; set; } //What sort of Doctor, Therapist, etc
         public ICollection<TimeSlot>? TimeSlots { get; set; }
+
     }
 }
