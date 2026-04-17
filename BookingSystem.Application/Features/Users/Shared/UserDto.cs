a7897a45ebb63b4f15f178a5f3fd3ea62c5f9a33
AshrafPatel <4shr4f@live.co.uk>
2026-04-17 15:41:08 +0100

    Creating Auth & User Dtos iitial start

---
+++ BookingSystem.Application/Features/Users/Shared/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Application.Features.Users.Shared
+{
+    public class UserDto
+    {
+        public required string Id { get; set; }
+        public required string Email { get; set; }
+    }
+}
--- BookingSystem.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BookingApplication.Domain
-{
-    public class Class1
-    {
-
-    }
-}
